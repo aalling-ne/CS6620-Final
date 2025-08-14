bf934fc1ba6c27b0577631dececf304dfcfc0cce
aalling-ne <alling.a@northeastern.edu>
2025-08-14 16:43:16 -0400

    explicitly defined terraform required_providers in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.50"
+    }
+  }
+}
+
 provider "aws" {
   region                      = "us-east-1"
   access_key                  = "dummy"
