d3e2c44317c5258bcb0c465c3e1f24edaa3c8598
aalling-ne <alling.a@northeastern.edu>
2025-08-14 16:57:04 -0400

    skip account_id for AWS, since we're using localstack

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,8 +12,10 @@ provider "aws" {
   region                      = "us-east-1"
   access_key                  = "dummy"
   secret_key                  = "dummy"
+  s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
   endpoints {
     s3       = "http://localhost:4566"
     iam      = "http://localhost:4566"
